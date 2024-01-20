e9f9286dc1a57b22b4c1e8f27d0edff83e48ab8b
samet <sametcn99@gmail.com>
2024-01-20 12:02:06 +0300

    initial commit

---
+++ background.js
@@ -0,0 +1,65 @@
+/**
+* Callback function that is called when the browser action is clicked.
+* Gets the GitHub username from the current tab URL, constructs a GitHub
+* Profile Viewer URL with that username, and opens that URL in a new tab.
+* Logs messages to console for status and any errors.
+*/
+chrome.action.onClicked.addListener(async (tab) => {
+
+    let username;
+
+    // Check if URL is a normal GitHub URL
+    if (isGitHubURL(tab.url)) {
+        username = getGitHubUsername(tab.url);
+    }
+    // Check if URL is a Gist URL
+    else if (isGistURL(tab.url)) {
+        username = getGistUsername(tab.url);
+    }
+
+    if (!username) {
+        console.log("Could not get GitHub username from URL");
+        return;
+    }
+
+    const gpvURL = `https://next-github-profile-viewer.vercel.app/${username}`;
+    await chrome.tabs.create({ url: gpvURL });
+    console.log("Redirected to GPV");
+
+});
+
+
+/**
+ * Checks if the given URL is a GitHub URL.
+ * Returns true if the URL matches the GitHub URL pattern, false otherwise.
+ */
+function isGitHubURL(url) {
+    return /^https?:\/\/github\.com\/.+/.test(url);
+}
+
+/**
+ * Checks if the given URL is a Gist URL.
+ * Returns true if the URL matches the Gist URL pattern, false otherwise.
+*/
+function isGistURL(url) {
+    return /^https?:\/\/gist\.github\.com\/.+/.test(url) || /^[a-fA-F0-9]{32}$/.test(url);
+}
+
+/**
+* Extracts the GitHub username from a normal GitHub URL.
+* Matches a GitHub URL pattern like github.com/username and returns
+* the username or undefined if no match.
+*/
+function getGitHubUsername(url) {
+    const matches = url.match(/github\.com\/([^/]+)\/?/);
+    return matches?.[1];
+}
+
+/**
+* Extracts the GitHub username from a Gist URL.
+* Matches a Gist URL pattern and returns the username.
+*/
+function getGistUsername(url) {
+    const matches = url.match(/gist\.github\.com\/([^/]+)\//);
+    return matches?.[1];
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# GitHub Profile Viewer Opener Chrome Extension
+
+This Chrome extension allows you to easily open a GitHub Profile on [Github Profile Viewer](https://next-github-profile-viewer.vercel.app).
+
+## Installation
+
+1. Clone or download this repository or 
+
+    ```bash
+    git clone https://github.com/sametcn99/open-on-gpv-crx.git
+    ```
+
+2. Open Google Chrome and go to `chrome://extensions/`.
+
+3. Enable "Developer mode" in the top right corner.
+
+4. Click on "Load unpacked" and select the extension directory.
+
+5. The extension icon should appear in your Chrome toolbar.
+
+## Easiest Way to Installation
+
+1. Download crx file from [Releases](https://github.com/sametcn99/open-on-gpv-crx/releases).
+2. Open Google Chrome and go to `chrome://extensions/`.
+3. Enable "Developer mode" in the top right corner.
+4. Drag and drop `.crx` file to the extensions page.
+5. And click `Add Extension` button.
+
+## How to Use
+
+1. Navigate to any GitHub or Gist profile page (e.g. `https://github.com/username/*` , `https://gist.github.com/username/*`).
+
+2. Click on the extension icon in the toolbar.
+
+3. The profile will be opened in  [Github Profile Viewer](https://next-github-profile-viewer.vercel.app) in a new tab.
+
+## Features
+
+- Quickly open GitHub Profiles in GPV.
+
+## Issues and Contributions
+
+Feel free to open issues or contribute to this project. Pull requests are welcome!
